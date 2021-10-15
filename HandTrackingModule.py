e1996e97365a65abe4739b6f42131d2978465a03
Josh Trivedi <josh.trivedi@gmail.com>
2021-10-15 23:51:01 +0530

    HandTrackingModule update, position of landmarks

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -31,7 +31,21 @@ class handDetector():
 
         return img
 
+    def findPosition(self, img, handNo=0, draw=False):
+        self.lmList = []
 
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+
+            for id, lm in enumerate(myHand.landmark):
+                h,w,c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                # print(id, cx, cy)
+                self.lmList.append([id, cx,cy])
+                if draw:
+                    #if id == 4
+                    cv2.circle(img, (cx,cy), 11, (255, 0, 255), cv2.FILLED)
+        return self.lmList
 
     def findDistance(self, p1, p2, img, draw=True, r=15, t=3):
         x1, y1 = self.lmList[p1][1:]
